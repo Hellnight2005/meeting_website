ebb8b6fbc41f4eda96e1c50cae45300f103bb973
Abhijeet <abhijeet2005shinde@gmail.com>
2025-04-18 22:51:14 +0530

    update it

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -1,20 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import { checkAuthType } from "@/middleware/vaildateUser";
+import { checkAuthType } from "@/middleware/vaildateUser"; // fixed typo
 
 const prisma = new PrismaClient();
 
-async function handler(req, { params }) {
+async function handler(req, { params, user }) {
   const { id } = params;
 
-  const user = await prisma.user.findUnique({ where: { id } });
+  const User = await prisma.user.findUnique({ where: { id } });
 
-  if (!user) {
+  if (!User) {
     return new Response(JSON.stringify({ message: "User not found" }), {
       status: 404,
     });
   }
 
-  return new Response(JSON.stringify(user), {
+  return new Response(JSON.stringify({ User }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
