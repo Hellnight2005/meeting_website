c5f34fc2eb35d27cd4920fc1688dff6ba89f16d0
Abhijeet <abhijeet2005shinde@gmail.com>
2025-05-01 22:49:06 +0530

    commit

--- src/app/api/Meeting/meeting_by_id/[id]/route.js
+++ src/app/api/Meeting/meeting_by_id/[id]/route.js
@@ -1,10 +1,12 @@
-// /pages/api/meeting/[id].js
+// app/api/meeting/meeting_by_id/[id]/route.js
+
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-// Handle GET request to fetch a specific meeting by ID
-export async function GET(req, { params }) {
+// GET /api/meeting/meeting_by_id/[id]
+export async function GET(request, { params }) {
   const { id } = params;
 
   if (!id) {
@@ -26,36 +28,6 @@ export async function GET(req, { params }) {
     return NextResponse.json({ success: true, data: meeting });
   } catch (error) {
     console.error("Error fetching meeting:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
-
-// Handle POST request to fetch meeting details using the meeting ID
-export async function POST(req) {
-  const { meetingId } = await req.json(); // Extract meeting ID from the body
-
-  if (!meetingId) {
-    return NextResponse.json(
-      { error: "Meeting ID is required" },
-      { status: 400 }
-    );
-  }
-
-  try {
-    const meeting = await prisma.meeting.findUnique({
-      where: { id: meetingId },
-    });
-
-    if (!meeting) {
-      return NextResponse.json({ error: "Meeting not found" }, { status: 404 });
-    }
-
-    return NextResponse.json({ success: true, data: meeting });
-  } catch (error) {
-    console.error("Error fetching meeting data:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
